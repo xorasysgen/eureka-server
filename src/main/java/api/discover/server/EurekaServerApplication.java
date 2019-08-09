05ed52c70237023bcdc85c9a704d14e8af2ac97d
sushil kumar bhaskar <xorasysgen@yahoo.com>
2019-08-09 12:10:11 +0530

    @Spring Cloud Eureka- Eureka-Sever

---
+++ src/main/java/api/discover/server/EurekaServerApplication.java
@@ -0,0 +1,17 @@
+package api.discover.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.PropertySource;
+
+@SpringBootApplication
+@PropertySource(ignoreResourceNotFound = true, value = "classpath:application.properties")
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+
+}
